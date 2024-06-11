5cf83155722b814040830ee23a365d5e05de05d3
Gabriele-Mazzoleni <g.mazzoleni29@studenti.unibg.it>
2024-06-11 15:00:20 +0200

    Create talk (Mazzoleni).js

---
+++ Parte 3/Get_Watch_next_by_Id/talk (Mazzoleni).js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const watch_next_schema = new mongoose.Schema({
+  _id: String,
+  Related_videos: [{
+    related_video_ids: String,
+    related_video_title: String,
+    related_presentedBy: String
+  }]
+
+}, { collection: 'tedx_data' });
+
+module.exports = mongoose.model('talk', watch_next_schema);
