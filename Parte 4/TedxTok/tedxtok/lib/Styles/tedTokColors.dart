d0a696e0ae7e17ff8b67974288690e6e3418baf5
Gabriele-Mazzoleni <g.mazzoleni29@studenti.unibg.it>
2024-07-04 17:03:58 +0200

    Build Mazzoleni mainPage

---
+++ Parte 4/TedxTok/tedxtok/lib/Styles/tedTokColors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+class tedTokColors{
+  tedTokColors._();
+
+  static const Color tedRed= Color.fromRGBO(232, 44, 44, 1);
+  static const Color tokBlue= Color.fromARGB(255, 94, 193, 204);
+
+}
--- Parte 4/TedxTok/tedxtok/lib/main.dart
+++ Parte 4/TedxTok/tedxtok/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tedxtok/Styles/TedTokColors.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
@@ -23,73 +22,166 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _isChecked1 = false;
-  bool _isChecked2 = false;
-  bool _isChecked3 = false;
+  // Lista dei topic, per la build demo iniziamo con 20 argomenti possibili
+  final List<String> topics = [
+    'TED Membership',
+    'evolution',
+    'wildlife',
+    'aliens',
+    'bionics',
+    'painting',
+    'personal growth',
+    'war',
+    'augmented reality',
+    'communication',
+    'crime',
+    'encryption',
+    'medical research',
+    'nuclear energy',
+    'ocean',
+    'religion',
+    'sleep',
+    'visualizations',
+    'beauty',
+    'metaverse',
+  ];
+
+  // Lista di booleani per tenere traccia dello stato delle checkbox
+  late List<bool> checkedTopics;
+
+  @override
+  void initState() {
+    super.initState();
+    // Inizializza la lista dei booleani con 'false'
+    checkedTopics = List<bool>.filled(topics.length, false);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Titolo'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
+    return SafeArea(
+      child: Column(
+        children: [
+          // Header
+          Container(
+            color: tedTokColors.tedRed, //Rosso TedTok
+            padding: EdgeInsets.symmetric(vertical: 20),
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _isChecked1,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _isChecked1 = value ?? false;
-                        });
-                      },
-                    ),
-                    Text('Checkbox 1'),
-                  ],
+                Text(
+                  'WELCOME TO',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _isChecked2,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _isChecked2 = value ?? false;
-                        });
-                      },
-                    ),
-                    Text('Checkbox 2'),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: _isChecked3,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _isChecked3 = value ?? false;
-                        });
-                      },
-                    ),
-                    Text('Checkbox 3'),
-                  ],
+                SizedBox(height: 5),
+                Image.asset(
+                  'assets/images/Logo_Bianco.png', // Path to the image asset
+                  height: 50, // Adjust the height as needed
                 ),
               ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Aggiungi qui l'azione del bottone
-                print('Bottone premuto');
-              },
-              child: Text('Premi'),
+          ),
+          SizedBox(height: 20),
+          // Instructions
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'Please select a list of topics that might interest you',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+          // List of Topics
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListView.builder(
+                  itemCount: topics.length,
+                  itemBuilder: (context, index) {
+                    return topicItem(
+                      topic: topics[index],
+                      isChecked: checkedTopics[index],
+                      onChanged: (bool? value) {
+                        setState(() {
+                          checkedTopics[index] = value ?? false;
+                        });
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          // Button
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: tedTokColors.tokBlue, // Azzurro TedTok
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 15),
+              ),
+              onPressed: () {
+                // Funzione di OnPressed, la aggiungeremo più tardi
+              },
+              child: Center(
+                child: Text(
+                  "LET'S BEGIN",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
+
+class topicItem extends StatelessWidget {
+  final String topic;
+  final bool isChecked;
+  final ValueChanged<bool?> onChanged;
+
+  topicItem({required this.topic, required this.isChecked, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          Checkbox(
+            value: isChecked,
+            onChanged: onChanged,
+            activeColor: tedTokColors.tokBlue
+          ),
+          SizedBox(width: 10),
+          Text(
+            topic,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
