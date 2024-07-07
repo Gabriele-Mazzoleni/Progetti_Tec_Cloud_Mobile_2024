3dad0908b52c9b696025a4f1ec651d0aa3845008
Gabriele-Mazzoleni <g.mazzoleni29@studenti.unibg.it>
2024-07-07 17:23:34 +0200

    Dati talks nelle pagine!

--- Parte 4/TedxTok/tedxtok/lib/secondPage.dart
+++ Parte 4/TedxTok/tedxtok/lib/secondPage.dart
@@ -19,7 +19,7 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
-  //late Future<List<Talk>> _talks;
+  late Future<List<Talk>> _talks;
   late VideoPlayerController _videoPlayerController;
   late ChewieController _chewieController;
 
@@ -27,11 +27,10 @@ class _SecondPageState extends State<SecondPage> {
   void initState() {
     super.initState();
     _initVideoPlayer();
-    //_talks= getTalkstByTagList(widget.selectedTags);
-    
+    _talks = getTalkstByTagList(widget.selectedTags);
   }
 
-void _initVideoPlayer() async {
+  void _initVideoPlayer() async {
     try {
       _videoPlayerController = VideoPlayerController.networkUrl(
         Uri.parse(
@@ -63,8 +62,7 @@ void _initVideoPlayer() async {
     super.dispose();
   }
 
-    void _launchUrl() async {
-    final url = 'https://www.ted.com/talks/jessie_christiansen_what_the_discovery_of_exoplanets_reveals_about_the_universe';
+  void _launchUrl(String url) async {
     // ignore: deprecated_member_use
     if (await canLaunch(url)) {
       // ignore: deprecated_member_use
@@ -76,106 +74,117 @@ void _initVideoPlayer() async {
 
   @override
   Widget build(BuildContext context) {
-    if (_videoPlayerController.value.isInitialized) {
-      return Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            //header
-            Container( 
-              decoration: const BoxDecoration(
-                color: tedTokColors.tedRed, // Rosso TedTok
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(sizes.stdRoundedCorner),
-                  bottomRight: Radius.circular(sizes.stdRoundedCorner),
-                ),
-              ),
-              padding: EdgeInsets.all(20.0),
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      //icon: Icon(Icons.add_box_outlined, color: Colors.whitesize: sizes.iconSize,), 
-                      icon: Icon(Icons.arrow_back_rounded, color: Colors.white, size: sizes.iconSize,), 
-                    ),
-                  ),
-                  Expanded(
-                    child: Center(
-                      child: Image.asset(
-                        'assets/images/Logo_Bianco.png', // Path to the image asset
-                        height: sizes.imgSize,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: FutureBuilder<List<Talk>>(
+        future: _talks,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(
+              child: Text('No talks available'),
+            );
+          } else {
+            List<Talk> talks = snapshot.data!;
+            return PageView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: talks.length,
+              itemBuilder: (context, index) {
+                Talk talk = talks[index];
+                return Column(
+                  children: [
+                    //header
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: tedTokColors.tedRed, // Rosso TedTok
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(sizes.stdRoundedCorner),
+                          bottomRight: Radius.circular(sizes.stdRoundedCorner),
+                        ),
+                      ),
+                      padding: EdgeInsets.all(20.0),
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(Icons.arrow_back_rounded, color: Colors.white, size: sizes.iconSize),
+                            ),
+                          ),
+                          Expanded(
+                            child: Center(
+                              child: Image.asset(
+                                'assets/images/Logo_Bianco.png', // Path to the image asset
+                                height: sizes.imgSize,
+                              ),
+                            ),
+                          ),
+                          const Align(
+                            alignment: Alignment.centerRight,
+                            child: Icon(Icons.send_rounded, color: Colors.white, size: sizes.iconSize),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  const Align(
-                    alignment: Alignment.centerRight,
-                    child: Icon(Icons.send_rounded, color: Colors.white, size:sizes.iconSize),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.65,
-              child: AspectRatio(
-                aspectRatio: _videoPlayerController.value.aspectRatio,
-                child: Chewie(
-                  controller: _chewieController,
-                ),
-              ),
-            ),
-            
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                color: tedTokColors.tokBlue, // Rosso TedTok
-                borderRadius: BorderRadius.only(
-                   topLeft: Radius.circular(sizes.stdRoundedCorner),
-                   topRight: Radius.circular(sizes.stdRoundedCorner),
-                  ),
-                ),
-                width: double.infinity,
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sample video title',
-                      style: fontStyles.TalkTitle,
+                    Container(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.65,
+                      child: AspectRatio(
+                        aspectRatio: _videoPlayerController.value.aspectRatio,
+                        child: Chewie(
+                          controller: _chewieController,
+                        ),
+                      ),
                     ),
-                    Text(
-                      'Sample video description',
-                      style:fontStyles.TalkSubitle,
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: tedTokColors.tokBlue, // Rosso TedTok
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(sizes.stdRoundedCorner),
+                            topRight: Radius.circular(sizes.stdRoundedCorner),
+                          ),
+                        ),
+                        width: double.infinity,
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              talk.title,
+                              style: fontStyles.TalkTitle,
+                            ),
+                            Text(
+                              talk.mainSpeaker,
+                              style: fontStyles.TalkSubitle,
+                            ),
+                            ElevatedButton(
+                              onPressed: () => _launchUrl(talk.url),
+                              child: Text('Visit TED.com'),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    ElevatedButton(
-                      onPressed: _launchUrl,
-                      child: Text('Visit TED.com'),
-                    ),
-                  ], 
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Image.asset(
-                  'assets/images/Logo_Bianco.png', // Path to the image asset
-                  height: sizes.imgSize*2,
-                ),
-        ),
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
+                  ],
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
 }
