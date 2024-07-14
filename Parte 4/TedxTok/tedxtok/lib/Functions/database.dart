2ea5dcc74113fd990533147690df40759ad00e10
GabrieleMasinari27 <g.masinari@studenti.unibg.it>
2024-07-14 16:58:09 +0200

    sistemazione caricamento tag

--- Parte 4/TedxTok/tedxtok/lib/Functions/database.dart
+++ Parte 4/TedxTok/tedxtok/lib/Functions/database.dart
@@ -1,13 +1,16 @@
-
 import 'package:mongo_dart/mongo_dart.dart';
 
 class Database {
   static late Db _db;
+  static bool _isInitialized = false;
 
   static Future<void> init() async {
-    _db = await Db.create(
-        'mongodb+srv://gmasinari:-PNDMDB-@mycluster.fvvxa1s.mongodb.net/unibg_tedx_2024');
-    await _db.open();
+    if (!_isInitialized) {
+      _db = await Db.create(
+          'mongodb+srv://gmasinari:-PNDMDB-@mycluster.fvvxa1s.mongodb.net/unibg_tedx_2024');
+      await _db.open();
+      _isInitialized = true;
+    }
   }
 
   static Future<List<String>> getTopics() async {
--- Parte 4/TedxTok/tedxtok/lib/topicSelectionPage.dart
+++ Parte 4/TedxTok/tedxtok/lib/topicSelectionPage.dart
@@ -16,9 +16,11 @@ class TopicSelectionPage extends StatefulWidget {
 }
 
 class _TopicSelectionPageState extends State<TopicSelectionPage> {
-  late List<String> topics=[];
+  late List<String> topics = [];
   late List<bool> checkedTopics;
   String searchQuery = '';
+  bool isLoading =
+      true; // Aggiungiamo una variabile di stato per il caricamento
 
   @override
   void initState() {
@@ -27,10 +29,11 @@ class _TopicSelectionPageState extends State<TopicSelectionPage> {
   }
 
   Future<void> _loadTopics() async {
-    await Database.init();
     topics = await Database.getTopics();
     checkedTopics = List<bool>.filled(topics.length, false);
-    setState(() {});
+    setState(() {
+      isLoading = false; // Impostiamo isLoading su false dopo il caricamento
+    });
   }
 
   @override
@@ -42,149 +45,155 @@ class _TopicSelectionPageState extends State<TopicSelectionPage> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                color: tedTokColors.tedRed,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(sizes.stdRoundedCorner),
-                  bottomRight: Radius.circular(sizes.stdRoundedCorner),
-                ),
-              ),
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: isLoading
+            ? Center(
+                child:
+                    CircularProgressIndicator()) // Mostriamo un indicatore di caricamento
+            : Column(
                 children: [
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'WELCOME',
-                        style: fontStyles.headerText,
-                        textAlign: TextAlign.right,
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: tedTokColors.tedRed,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(sizes.stdRoundedCorner),
+                        bottomRight: Radius.circular(sizes.stdRoundedCorner),
                       ),
-                      Text(
-                        'TO',
-                        style: fontStyles.headerText,
-                        textAlign: TextAlign.right,
-                      ),
-                    ],
-                  ),
-                  Image.asset(
-                    'assets/images/Logo_Bianco.png',
-                    height: sizes.imgSize,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Hello, ${widget.userData.username}',
-                style: fontStyles.introText,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'please select a list of topics that might interest you',
-                style: fontStyles.introText,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius:
-                        BorderRadius.circular(sizes.smallRoundedCorner),
-                  ),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search for a topic',
-                          border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.circular(sizes.smallRoundedCorner),
-                          ),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'WELCOME',
+                              style: fontStyles.headerText,
+                              textAlign: TextAlign.right,
+                            ),
+                            Text(
+                              'TO',
+                              style: fontStyles.headerText,
+                              textAlign: TextAlign.right,
+                            ),
+                          ],
                         ),
-                        onChanged: (value) {
-                          setState(() {
-                            searchQuery = value;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: filteredTopics.length,
-                          itemBuilder: (context, index) {
-                            String topic = filteredTopics[index];
-                            int originalIndex = topics.indexOf(topic);
-                            return topicItem(
-                              topic: topic,
-                              isChecked: checkedTopics[originalIndex],
-                              onChanged: (bool? value) {
+                        Image.asset(
+                          'assets/images/Logo_Bianco.png',
+                          height: sizes.imgSize,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'Hello, ${widget.userData.username}',
+                      style: fontStyles.introText,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'Please select a list of topics that might interest you',
+                      style: fontStyles.introText,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius:
+                              BorderRadius.circular(sizes.smallRoundedCorner),
+                        ),
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search for a topic',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      sizes.smallRoundedCorner),
+                                ),
+                              ),
+                              onChanged: (value) {
                                 setState(() {
-                                  checkedTopics[originalIndex] = value ?? false;
+                                  searchQuery = value;
                                 });
                               },
-                            );
-                          },
+                            ),
+                            SizedBox(height: 20),
+                            Expanded(
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: filteredTopics.map((topic) {
+                                    int originalIndex = topics.indexOf(topic);
+                                    return topicItem(
+                                      topic: topic,
+                                      isChecked: checkedTopics[originalIndex],
+                                      onChanged: (bool? value) {
+                                        setState(() {
+                                          checkedTopics[originalIndex] =
+                                              value ?? false;
+                                        });
+                                      },
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: tedTokColors.tokBlue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(sizes.stdRoundedCorner),
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                ),
-                onPressed: () {
-                  List<String> selectedTopics = [];
-                  for (int i = 0; i < topics.length; i++) {
-                    if (checkedTopics[i]) {
-                      selectedTopics.add(topics[i]);
-                    }
-                  }
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          TalkDisplayPage(selectedTags: selectedTopics),
                     ),
-                  );
-                },
-                child: const Center(
-                  child: Text(
-                    "LET'S BEGIN",
-                    style: fontStyles.buttonText,
                   ),
-                ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: tedTokColors.tokBlue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(sizes.stdRoundedCorner),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      onPressed: () {
+                        List<String> selectedTopics = [];
+                        for (int i = 0; i < topics.length; i++) {
+                          if (checkedTopics[i]) {
+                            selectedTopics.add(topics[i]);
+                          }
+                        }
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                TalkDisplayPage(selectedTags: selectedTopics),
+                          ),
+                        );
+                      },
+                      child: const Center(
+                        child: Text(
+                          "LET'S BEGIN",
+                          style: fontStyles.buttonText,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
-            ),
-            SizedBox(height: 20),
-          ],
-        ),
       ),
     );
   }
